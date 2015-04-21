020106ce183eebc7b1c716bd12b7077e0e356ecd
alif chowdhury <ifthesim@gmail.com>
2015-04-21 09:46:22 -0400

    gg

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20150420220040) do
   end
 
   create_table "signs", force: :cascade do |t|
-    t.text     "image"
+    t.text     "sign_text"
     t.integer  "student_id"
     t.integer  "adult_id"
     t.datetime "created_at", null: false
