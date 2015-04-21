aeba94e963cf57442cb7392ca007fdc2a5c3f66f
presto2116 <preston.straight@gmail.com>
2015-04-21 11:49:49 -0400

    students pages

--- db/migrate/20150420225523_create_questions.rb
+++ db/migrate/20150420225523_create_questions.rb
@@ -1,7 +1,17 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.text :question    
+    	t.belongs_to :student
+      t.boolean :q1
+      t.boolean :q2
+      t.boolean :q3
+      t.boolean :q4
+      t.boolean :q5
+      t.boolean :q6
+      t.boolean :q7
+      t.boolean :q8
+      t.boolean :q9
+      t.boolean :q10  
       t.timestamps null: false
     end
   end
