c810097ed67dee705889d836de68c7badb804740
presto2116 <preston.straight@gmail.com>
2015-04-21 09:50:11 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+root 'signs#index'
+
+resources :students do
+    resources :questions
+  end
+
+  resources :signs
+
+  resources :adults
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
