22b87cba36efd17131c261588d5aaaf690f17a93
presto2116 <preston.straight@gmail.com>
2015-04-22 12:00:32 -0400

    heroku problems

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,12 +12,12 @@ class StudentsController < ApplicationController
 
 	def new
 		@student = Student.new
-	end
-
+	end	
+	
 	def create
 		@student= Student.new (student_params)
 		if @student.save
-			redirect_to "/students/#{@student.id}"
+			redirect_to student_path(@student)
 		else
 			render :new
 		end
